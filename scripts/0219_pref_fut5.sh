ad01886b49ef2d6fcb9c5a05d0f84f4db8ecc1b6
victorique <victorique1929@gmail.com>
2025-02-24 18:29:21 -0800

    write prefbash

--- scripts/0219_pref_fut5.sh
+++ scripts/0219_pref_fut5.sh
@@ -6,17 +6,16 @@ seeds=(0 100 200 300 0 100 200 300)
 
 
 # Loop over each GPU
-for i in {5..8}
+for i in {0..4}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python /home/caihy/pvp/pvp/experiments/metadrive/train_pref_metadrive_fakehuman.py \
-    --exp_name=see5 \
+    --exp_name=see15 \
     --wandb_project=cpl \
     --wandb_team=victorique \
     --seed=${seeds[$i]} \
-    --trial_name=see5 \
-    --takeover_see=5 \
-    --free_level=0.5 \
-    > "0219_see5_seed${seeds[$i]}.log" 2>&1 &
+    --trial_name=see15 \
+    --free_level=0.9 \
+    > "0224_see5_seed${seeds[$i]}.log" 2>&1 &
 done
 
--- scripts/0219_pref_fut5cbias1.sh
+++ scripts/0219_pref_fut5cbias1.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 # Define the seeds for each GPU
-seeds=(0 100 200 300 0 100 200 300 0 100 200 300)
+seeds=(0 100 200 300 0 100 200 300)
 
 
 
 # Loop over each GPU
-for i in {0..2}
+for i in {5..8}
 do
     CUDA_VISIBLE_DEVICES=$i \
     nohup python /home/caihy/pvp/pvp/experiments/metadrive/train_pref_metadrive_fakehuman.py \
-    --exp_name=see5 \
+    --exp_name=see15cbias1 \
     --wandb_project=cpl \
     --wandb_team=victorique \
     --seed=${seeds[$i]} \
-    --trial_name=see5cbias1 \
-    --takeover_see=5 \
-    --free_level=0.5 \
+    --trial_name=see15cbias1 \
+    --free_level=0.9 \
     --cbias=1 \
-    > "0219_see5cbias1_seed${seeds[$i]}.log" 2>&1 &
+    > "0219_see5_seed${seeds[$i]}.log" 2>&1 &
 done
-
