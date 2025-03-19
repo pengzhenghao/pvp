7e6645b635a0809b5406893232b535803d7fc0ff
caihy <haoyuan@cs.ucla.edu>
2025-03-19 14:58:29 -0700

    minor

--- pvp/experiments/metadrive/train_pvp_metadrive_fakehuman_fakepos_cpl.py
+++ pvp/experiments/metadrive/train_pvp_metadrive_fakehuman_fakepos_cpl.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
     ]
     
     if args.cpl_loss_weight == 0:
-        wbname = "baseline:HG" + get_time_str()
+        wbname = "baseline_HG" + get_time_str()
     else:
         wbname = "CPL_" + "d=" + str(args.stop_img_samples) + "w=" + str(args.imgfuturesteps) + "bc=" + str(args.bc_loss_weight) + "bias=" + str(args.bias) + "_" + get_time_str()
         #add alpha=0.1, bias=0.5 to wbname
