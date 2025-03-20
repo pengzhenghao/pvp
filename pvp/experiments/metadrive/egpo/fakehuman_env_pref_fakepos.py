bd68f0d00cf7dbd413a7ba5a8c3ce5514e195d8c
caihy <haoyuan@cs.ucla.edu>
2025-03-19 17:47:27 -0700

    5 -> 10

--- pvp/experiments/metadrive/egpo/fakehuman_env_pref_fakepos.py
+++ pvp/experiments/metadrive/egpo/fakehuman_env_pref_fakepos.py
@@ -533,7 +533,7 @@ class FakeHumanEnvPref(HumanInTheLoopEnv):
                 break
         self.set_state(saved_state)
         from pvp.sb3.common.utils import safe_mean
-        if total_reward > 5:
+        if total_reward > 10:
             #total_reward += values_n.item()
             pass
         else:
