5149bd581ca597d45d8771192c28f41a65562d6b
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-05-14 17:41:04 -0700

    only for drawing

--- pvp/experiments/robosuite/egpo/fakehuman_env.py
+++ pvp/experiments/robosuite/egpo/fakehuman_env.py
@@ -425,7 +425,9 @@ class CustomWrapper(gym.Env):
         if info_real["mean_action_diff"] > self.config["switch_to_expert"]:
             self.rec.append(info_real2["total_reward"] - info_real["total_reward"])
         # return info_real["mean_action_diff"] > self.config["switch_to_expert"] 
-        return (info_real2["total_reward"] - info_real["total_reward"] > self.config["switch_to_expert"]) or (info_real["total_reward"] < 2)
+        
+        return True
+        # return (info_real2["total_reward"] - info_real["total_reward"] > self.config["switch_to_expert"]) or (info_real["total_reward"] < 2)
     
     def store_preference_pairs(self, predicted_traj, future_steps_preference, expert_action):
         for step in range(min(len(predicted_traj) - 1, future_steps_preference)):
@@ -463,6 +465,7 @@ class CustomWrapper(gym.Env):
         #     # if len(self.rec) > 0:
         #     #     print("MEAN:", np.mean(self.rec))
         
+        time.sleep(1)
         self.takeover = (np.mean((self.agent_action - expert_action) ** 2) > 0.01) and not self.config["eval"]
         
         self.takeover2 = (expert_action[-1] * action_[-1] < 0) and not self.config["eval"]
--- pvp/experiments/robosuite/train_comb_metadrive_fakehuman.py
+++ pvp/experiments/robosuite/train_comb_metadrive_fakehuman.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
             # window_size=(1600, 1100),
 
             # FakeHumanEnv config:
-            use_render=False,
+            use_render=True,
             switch_to_expert=args.switch_to_expert,
             future_steps_predict=args.future_steps_predict,
             update_future_freq=args.update_future_freq,
