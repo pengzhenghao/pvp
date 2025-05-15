3850fd92e61155fcfb660dc6fac5186437a55ef9
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-05-15 14:10:15 -0700

    no sleep

--- pvp/experiments/robosuite/egpo/fakehuman_env.py
+++ pvp/experiments/robosuite/egpo/fakehuman_env.py
@@ -465,7 +465,6 @@ class CustomWrapper(gym.Env):
         #     # if len(self.rec) > 0:
         #     #     print("MEAN:", np.mean(self.rec))
         
-        time.sleep(1)
         self.takeover = (np.mean((self.agent_action - expert_action) ** 2) > 0.01) and not self.config["eval"]
         
         self.takeover2 = (expert_action[-1] * action_[-1] < 0) and not self.config["eval"]
