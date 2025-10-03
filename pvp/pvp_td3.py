cda730f116ff23f0091cf531d22ca3aeaca1cc11
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-10-03 14:35:59 -0700

    add good init -> high perf

--- pvp/pvp_td3.py
+++ pvp/pvp_td3.py
@@ -313,7 +313,10 @@ class PVPTD3(TD3):
             assert load_buffer, "warmup is useful only when load buffer"
             print("Start warmup with steps: " + str(warmup_steps))
             self.train(batch_size=self.batch_size, gradient_steps=warmup_steps)
-
+        policy_path = f"/home/caihy/pvp/runs/Ours_bcw=1.0_0926/Ours_bcw=1.0_0926_4f3847d4/models/rl_model_8000_steps/policy.pth"
+        policy_weights = torch.load(policy_path, map_location=self.device)
+        self.policy.load_state_dict(policy_weights)
+        next_upd = 10000
         while self.num_timesteps < total_timesteps:
             rollout = self.collect_rollouts(
                 self.env,
