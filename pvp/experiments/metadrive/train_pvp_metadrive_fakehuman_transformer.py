0881f2377ee0f37cd3240f602a9b2a524fed994c
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-10-08 11:10:16 -0700

    revise transformer trainer

--- pvp/experiments/metadrive/train_pvp_metadrive_fakehuman_transformer.py
+++ pvp/experiments/metadrive/train_pvp_metadrive_fakehuman_transformer.py
@@ -473,25 +473,26 @@ class HistoryWrapper(gym.Wrapper):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--exp_name", default="pvp_metadrive_fakehuman", type=str, help="The name for this batch of experiments."
+        "--exp_name", default="pvp_transformer", type=str, help="The name for this batch of experiments."
     )
     parser.add_argument("--learning_starts", default=200, type=int)
     parser.add_argument("--save_freq", default=500, type=int)
     parser.add_argument("--seed", default=0, type=int, help="The random seed.")
     parser.add_argument("--wandb", action="store_true", help="Set to True to upload stats to wandb.")
-    parser.add_argument("--wandb_project", type=str, default="", help="The project name for wandb.")
-    parser.add_argument("--wandb_team", type=str, default="", help="The team name for wandb.")
-    parser.add_argument("--log_dir", type=str, default="/data/zhenghao/pvp", help="Folder to store the logs.")
+    parser.add_argument("--wandb_project", type=str, default="transformer", help="The project name for wandb.")
+    parser.add_argument("--wandb_team", type=str, default="victorique", help="The team name for wandb.")
+    parser.add_argument("--log_dir", type=str, default="/home/caihy/pvp/runs", help="Folder to store the logs.")
     parser.add_argument("--free_level", type=float, default=0.95)
     parser.add_argument("--bc_loss_weight", type=float, default=0.0)
-
+    parser.add_argument("--with_human_proxy_value_loss", default="True", type=str)
+    parser.add_argument("--with_agent_proxy_value_loss", default="True", type=str)
     # parser.add_argument(
     #     "--intervention_start_stop_td", default=True, type=bool, help="Whether to use intervention_start_stop_td."
     # )
 
     parser.add_argument("--adaptive_batch_size", default="False", type=str)
-    parser.add_argument("--ckpt", default="", type=str)
-
+    parser.add_argument("--ckpt", default="", type=str)   #/home/caihy/pvp/runs/runs/pvp_metadrive_fakehuman_freelevel0.95/pvp_metadrive_fakehuman_freelevel0.95_2025-10-05_12-15-40_08529fca/models/rl_model_50000_steps.zip
+    parser.add_argument("--only_bc_loss", default="False", type=str)
     parser.add_argument("--toy_env", action="store_true", help="Whether to use a toy environment.")
     # parser.add_argument(
     #     "--device",
@@ -546,7 +547,9 @@ if __name__ == '__main__':
             # intervention_start_stop_td=args.intervention_start_stop_td,
             adaptive_batch_size=args.adaptive_batch_size,
             bc_loss_weight=args.bc_loss_weight,
-            only_bc_loss="False",
+            only_bc_loss=args.only_bc_loss,
+            with_human_proxy_value_loss=args.with_human_proxy_value_loss,
+            with_agent_proxy_value_loss=args.with_agent_proxy_value_loss,
             add_bc_loss="True" if args.bc_loss_weight > 0.0 else "False",
             use_balance_sample=True,
             agent_data_ratio=1.0,
@@ -559,7 +562,7 @@ if __name__ == '__main__':
             # policy_kwargs=dict(net_arch=[256, 256]),
             policy=PVPContinuousPolicy,
             policy_kwargs=dict(
-                features_extractor_class=GRUFeatureExtractor,
+                features_extractor_class=BBoxFeatureExtractor,
                 features_extractor_kwargs=dict(
                     features_dim=256,
                     transformer_hidden_dim=512,
@@ -575,7 +578,7 @@ if __name__ == '__main__':
             learning_rate=1e-4,
             q_value_bound=1,
             optimize_memory_usage=True,
-            buffer_size=50_000,  # We only conduct experiment less than 50K steps
+            buffer_size=500_000,  # We only conduct experiment less than 50K steps
             learning_starts=args.learning_starts,  # The number of steps before
             batch_size=128,  # Reduce the batch size for real-time copilot
             tau=0.005,
@@ -632,7 +635,7 @@ if __name__ == '__main__':
         eval_env = Monitor(env=eval_env, filename=str(trial_dir))
         return eval_env
 
-    eval_env = SubprocVecEnv([_make_eval_env])
+    eval_env = SubprocVecEnv([_make_eval_env] * 5)
 
     # ===== Setup the callbacks =====
     save_freq = args.save_freq  # Number of steps per model checkpoint
@@ -663,7 +666,7 @@ if __name__ == '__main__':
     # ===== Launch training =====
     model.learn(
         # training
-        total_timesteps=50_000,
+        total_timesteps=500_000,
         callback=callbacks,
         reset_num_timesteps=True,
 
@@ -675,8 +678,8 @@ if __name__ == '__main__':
 
         # eval
         eval_env=eval_env,
-        eval_freq=500,
-        n_eval_episodes=10,
+        eval_freq=2000,
+        n_eval_episodes=200,
         eval_log_path=str(trial_dir),
 
         # logging
