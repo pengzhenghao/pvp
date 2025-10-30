3e43af0778808eb401a6251a6d21cf74a1c856c1
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-10-29 17:18:25 -0700

    asdads

--- pvp/experiments/metadrive/train_comb_metadrive_fakehuman.py
+++ pvp/experiments/metadrive/train_comb_metadrive_fakehuman.py
@@ -14,6 +14,9 @@ from pvp.sb3.td3.policies import TD3Policy
 from pvp.utils.shared_control_monitor import SharedControlMonitor
 from pvp.utils.utils import get_time_str
 import pathlib
+from metadrive.component.sensors.depth_camera import DepthCamera
+from pvp.sb3.sac.our_features_extractor import OurFeaturesExtractorCNN as OurFeaturesExtractor
+sensor_size = (42, 42)
 FOLDER_PATH = pathlib.Path(__file__).parent.parent
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -85,6 +88,11 @@ if __name__ == '__main__':
             update_future_freq=args.update_future_freq,
             future_steps_preference=args.future_steps_preference,
             expert_noise=args.expert_noise,
+                # FakeHumanEnv config:
+                image_observation=True, 
+                vehicle_config=dict(image_source="depth_camera"),
+                sensors={"depth_camera": (DepthCamera, *sensor_size)},
+                stack_size=3,
         ),
 
         # Algorithm config
@@ -213,6 +221,6 @@ if __name__ == '__main__':
         save_path_human="humanbuffer",
         save_path_replay="prefbuffer",
         load_buffer=True,
-        load_path_human="/home/caihy/pvp/humanbufferoffbc8k/human_buffer_50000.pkl",
-        load_path_replay="/home/caihy/pvp/prefbufferoffbc8k/replay_buffer_50000.pkl"
+        load_path_human="/home/caihy/pvp/CNN/human_buffer_20000.pkl",
+        load_path_replay="/home/caihy/pvp/CNN/replay_buffer_20000.pkl"
     )
